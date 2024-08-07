3afeb5503b1bb8c4364999ef88a6e7e6f073f60a
thsamaji <60818655+thsamajiki@users.noreply.github.com>
2024-08-08 01:39:36 +0900

    Create Game.h

---
+++ JetForce/Game.h
@@ -0,0 +1,31 @@
+#pragma once
+#pragma comment(lib, "Msimg32.lib")
+
+#include "1945_First.h"
+#include "framework.h"
+
+#include "Define.h"
+#include "Enum.h"
+#include "Struct.h"
+#include "Func.h"
+
+#include <crtdbg.h>
+
+#include <assert.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <string>
+
+using std::vector;
+using std::list;
+using std::map;
+using std::string;
+using std::wstring;
+using std::make_pair;
+using std::to_wstring;
+using std::remove;
+using std::cout;
+using std::cin;
+using std::endl;
