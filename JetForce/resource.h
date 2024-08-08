5b432c6584627d39a7758a28a4e2460082ff2e9b
thsamajiki <ant1102@naver.com>
2024-08-08 15:06:18 +0900

    resource 헤더 추가

--- JetForce/resource.h
+++ JetForce/resource.h
@@ -5,13 +5,13 @@
 #define IDS_APP_TITLE			103
 
 #define IDR_MAINFRAME			128
-#define IDD_MY1945FIRST_DIALOG	102
+#define IDD_JETFORCE			102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_MY1945FIRST			107
+#define IDI_JETFORCE			107
 #define IDI_SMALL				108
-#define IDC_MY1945FIRST			109
+#define IDC_JETFORCE			109
 #define IDC_MYICON				2
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
